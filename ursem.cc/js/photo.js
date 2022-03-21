255ac6e648893ddadbae3a0c54d7e5d55fbd0afe
Peter <peter@ursem.cc>
2022-03-21 12:35:47 -0600

    Dynamically remove filters from bar

--- ursem.cc/js/photo.js
+++ ursem.cc/js/photo.js
@@ -2,6 +2,12 @@
 //  V0.1 - March 2022
 //  Hello to anyone here!
 
+const colours = ['#cfc7fa', '#e2c6ee', '#efc8dd', '#d7e9db', '#ced9ed'];
+var imgSection = document.getElementById("imgs");
+var data = {};
+var tags = [];
+var possibleTags = [];
+
 fetch('images/database.json')
 .then(r => {
   r.json()
@@ -9,29 +15,19 @@ fetch('images/database.json')
     let tagHTML = [];
     result.tags.forEach(tag => {let tagStr ='"' + tag + '"'; tagHTML.push("<a href='javascript:void(0);' class='tags "+ tag +"' onclick='setTag(" + tagStr + ")'>" + tag + "</a> ");});
     document.getElementById("tagBar").innerHTML = tagHTML.join(' ');
+    data = result;
     printPage([]);
   });
 });
 
-var tags = [];
-var possibleTags = [];
-
 function setTag(tag) {
-  var colours = ['#cfc7fa', '#e2c6ee', '#efc8dd', '#d7e9db', '#ced9ed'];
-  let removeTags = tags.filter(tag => !possibleTags.includes(tag));
-
-  removeTags.forEach(tag => {
-    let tagButton = document.getElementById(tag);
-    tagButton.style.display = 'none';
-  });
-
-  index = tags.indexOf(tag);
+  let index = tags.indexOf(tag);
   var tagRef = document.getElementsByClassName(tag)[0];
+
   if (index === -1) {
     let tagColour = colours[Math.floor(Math.random()*colours.length)];
     tagRef.classList.add('selected');
     tagRef.style.setProperty('background-color', tagColour);
-    console.log(tags);
     tags.push(tag);
   }
   else {
@@ -40,36 +36,46 @@ function setTag(tag) {
     console.log(tagRef);
     tags.splice(index, 1);
   } 
+  possibleTags =[];
   printPage(tags);
+
+  let removeTags = data.tags.filter(tag => !possibleTags.includes(tag));
+  removeTags.forEach(tag => {
+    console.log('Removing Tag: ' + tag);
+    let tagButton = document.getElementsByClassName(tag)[0];
+    tagButton.style.display = 'none';
+  });
+  possibleTags.forEach(tag => {
+    let tagButton = document.getElementsByClassName(tag)[0];
+    tagButton.style.display = 'inline-block'
+  })
 }
 
 function printPage(filters, printHidden) {
-  var imgSection = document.getElementById("imgs");
-  var imgMatches = [];
-  var imgHTML = [];
-  fetch('images/database.json')
-  .then(response => {
-    response.json().then(result => {
-      imgMatches = result.images.filter(img => {
-        // Check that image matches all filters
-        if (img.tags.includes('Hidden')) return printHidden;
-        let filtered = [];
-        filters.forEach(filter => {
-        filtered.push(img['tags'].includes(filter));
-        });
-        let Return = filtered.every(bool => bool === true);
-        return Return;
-      });
+  let imgMatches = [];
+  let imgHTML = [];
 
-      imgMatches.forEach(img => {
-          possibleTags = img.tags
-          imgHTML.push("<img class='img' src='" + img.url + "'>");
-      });
-      imgSection.innerHTML = imgHTML.join(' ');
-      setModals();
+  imgMatches = data.images.filter(img => {
+    // Check that image matches all filters
+    if (img.tags.includes('Hidden')) return printHidden;
+    let filtered = [];
+    filters.forEach(filter => {
+      filtered.push(img['tags'].includes(filter));
     });
+    let Return = filtered.every(bool => bool === true);
+    return Return;
   });
+
+  imgMatches.forEach(img => {
+      img.tags.forEach(tag => {
+        if(!possibleTags.includes(tag)) possibleTags.push(tag);
+      })
+      imgHTML.push("<img class='img' src='" + img.url + "'>");
+  });
+  imgSection.innerHTML = imgHTML.join(' ');
+  setModals();
 }
+
 
 //MODAL OVERLAY
 
