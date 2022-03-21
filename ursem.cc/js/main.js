4265a690f2106f96774a87ed9703603e69cbc475
Peter <peter@ursem.cc>
2022-03-21 12:09:11 -0600

    Initial Commt

---
+++ ursem.cc/js/main.js
@@ -0,0 +1,3 @@
+//Cursor
+document.addEventListener('mousedown', function(){document.body.style.cursor = 'grabbing';});
+document.addEventListener('mouseup', function(){document.body.style.cursor = 'grab'});
