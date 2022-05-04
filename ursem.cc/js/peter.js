4d3fe95287719c7da92ec8f93c6dd6a1f609f52b
Peter <peter@ursem.cc>
2022-05-03 23:00:26 -0600

    Found an older copy of the site :)

---
+++ ursem.cc/js/peter.js
@@ -0,0 +1,21 @@
+// Get the modal
+var modal = document.getElementById("imgModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var imgs = document.getElementsByClassName("imgs");
+var modalImg = document.getElementById("modalImg");
+for(var img of imgs)
+{
+    img.onclick = function(){
+      modal.style.display = "block";
+      modalImg.src = this.src;
+    }
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
