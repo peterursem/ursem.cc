113deab0a4781d458db1c02e2963bf1747dfb373
Peter <peter@ursem.cc>
2022-07-05 20:55:12 -0600

    nGen canvas test
    This is a scene based on the time of day with birds for you to spawn.

--- ursem.cc/js/main.js
+++
@@ -1,5 +0,0 @@
-/* Copyright 2020 - 2022 Peter Ursem, All Rights Reserved */
-
-//Cursor
-document.addEventListener('mousedown', function(){document.body.style.cursor = 'grabbing';});
-document.addEventListener('mouseup', function(){document.body.style.cursor = 'grab'});
