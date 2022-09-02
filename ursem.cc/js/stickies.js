ae2fce79e7d27cef49be88ccd6e7190b023ea4a7
Peter <peter@ursem.cc>
2022-09-02 11:27:54 -0600

    Patching module location

--- ursem.cc/js/stickies.js
+++ ursem.cc/js/stickies.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set, onValue } from "firebase/database";
+import { initializeApp } from "/firebase/app";
+import { getDatabase, ref, set, onValue } from "/firebase/database";
 
 const firebaseConfig = {
   databaseURL: "https://ursemca.firebaseio.com/",
